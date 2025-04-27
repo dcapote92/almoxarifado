5c3926ab924e7313a9013130a5bedaae492b3719
vicap <danicapote92@gmail.com>
2025-04-27 15:39:39 -0300

    update: Providers added

--- src/main/kotlin/com/angerbytes/almoxarifado/config/TestConfig.kt
+++ src/main/kotlin/com/angerbytes/almoxarifado/config/TestConfig.kt
@@ -1,8 +1,10 @@
 package com.angerbytes.almoxarifado.config
 
 import com.angerbytes.almoxarifado.model.Product
+import com.angerbytes.almoxarifado.model.Provider
 import com.angerbytes.almoxarifado.model.User
 import com.angerbytes.almoxarifado.repositories.ProductRepository
+import com.angerbytes.almoxarifado.repositories.ProviderRepository
 import com.angerbytes.almoxarifado.repositories.UserRepository
 import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,6 +18,7 @@ import org.springframework.context.annotation.Profile
 class TestConfig(
     @field:Autowired private val userRepository: UserRepository,
     @field:Autowired private val productRepository: ProductRepository,
+    @field:Autowired private val providerRepository: ProviderRepository
 ) : CommandLineRunner{
 
 
@@ -24,6 +27,7 @@ class TestConfig(
 
         userRepository.deleteAll()
         productRepository.deleteAll()
+        providerRepository.deleteAll()
 
         val users = listOf(
             User(null,"Marc", "123.123.123-12","marc@gmail.com","(12)1234 1234"),
@@ -35,15 +39,18 @@ class TestConfig(
             Product(null,"124","123456781","Brutus", "Pinheiro deshidratado","lenha", "unidade")
         )
 
+        val providers = listOf(
+            Provider(null,"001","Juridico","000.000/0000-11","001","Bom Oleo","Olinho","Oliveira, 33","olivo@verde.com","Oliva Green","(11)1111 1111"),
+            Provider(null,"002","Juridico","000.000/0000-12","002","Blue Berry","Amora","Oliveira, 34","berry@blue.com","Berry Blue","(22)2222 2222")
+        )
+
         userRepository.saveAll(users)
         productRepository.saveAll(products)
+        providerRepository.saveAll(providers)
 
         userRepository.flush()
         productRepository.flush()
-
-        // Log para verificação
-        println("Users no banco: ${userRepository.findAll()}")
-        println("Products no banco: ${productRepository.findAll()}")
+        providerRepository.flush()
 
     }
 }
