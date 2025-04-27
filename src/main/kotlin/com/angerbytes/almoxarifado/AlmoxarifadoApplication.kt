a8a72df5bfada5f5d9602c4c4f77546449470c48
vicap <danicapote92@gmail.com>
2025-04-26 22:53:34 -0300

    update: JpaRepositories enabled

--- src/main/kotlin/com/angerbytes/almoxarifado/AlmoxarifadoApplication.kt
+++ src/main/kotlin/com/angerbytes/almoxarifado/AlmoxarifadoApplication.kt
@@ -1,9 +1,13 @@
 package com.angerbytes.almoxarifado
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories("com.angerbytes.almoxarifado.repositories")
+@EntityScan("com.angerbytes.almoxarifado.model")
 class AlmoxarifadoApplication
 
 fun main(args: Array<String>) {
