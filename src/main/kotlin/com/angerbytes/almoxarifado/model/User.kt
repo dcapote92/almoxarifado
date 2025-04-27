ed5bf3380bac5597114270e4a88393368dc0569e
vicap <danicapote92@gmail.com>
2025-04-26 22:59:22 -0300

    update: User class modified to by a entity

--- src/main/kotlin/com/angerbytes/almoxarifado/model/User.kt
+++ src/main/kotlin/com/angerbytes/almoxarifado/model/User.kt
@@ -1,9 +1,22 @@
 package com.angerbytes.almoxarifado.model
 
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "tb_user")
 data class User(
-    val id: Long,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
     var name: String,
     var cpf: String,
     var email: String,
     var phone: String
-)
+){
+    constructor(): this(null,"","","","")
+}
+
