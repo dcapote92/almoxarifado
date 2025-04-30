7c8972d67f56108275e9bbd14980a5dba5ff2441
vicap <danicapote92@gmail.com>
2025-04-30 20:32:39 -0300

    update: Location added to TestConfig.kt

--- src/main/kotlin/com/angerbytes/almoxarifado/config/TestConfig.kt
+++ src/main/kotlin/com/angerbytes/almoxarifado/config/TestConfig.kt
@@ -1,12 +1,17 @@
 package com.angerbytes.almoxarifado.config
 
 import com.angerbytes.almoxarifado.model.FiscalNote
+import com.angerbytes.almoxarifado.model.Location
 import com.angerbytes.almoxarifado.model.Product
 import com.angerbytes.almoxarifado.model.Provider
 import com.angerbytes.almoxarifado.model.User
 import com.angerbytes.almoxarifado.model.enums.FiscalNoteModel
 import com.angerbytes.almoxarifado.model.enums.FiscalNoteStatus
+import com.angerbytes.almoxarifado.model.enums.ProviderType
+import com.angerbytes.almoxarifado.model.enums.Sections
+import com.angerbytes.almoxarifado.model.enums.UnitMeasures
 import com.angerbytes.almoxarifado.repositories.FiscalNoteRepository
+import com.angerbytes.almoxarifado.repositories.LocationRepository
 import com.angerbytes.almoxarifado.repositories.ProductRepository
 import com.angerbytes.almoxarifado.repositories.ProviderRepository
 import com.angerbytes.almoxarifado.repositories.UserRepository
@@ -25,7 +30,8 @@ class TestConfig(
     @field:Autowired private val userRepository: UserRepository,
     @field:Autowired private val productRepository: ProductRepository,
     @field:Autowired private val providerRepository: ProviderRepository,
-    @field:Autowired private val fiscalNoteRepository: FiscalNoteRepository
+    @field:Autowired private val fiscalNoteRepository: FiscalNoteRepository,
+    @field:Autowired private val locationRepository: LocationRepository
 ) : CommandLineRunner{
 
 
@@ -36,6 +42,7 @@ class TestConfig(
         productRepository.deleteAll()
         providerRepository.deleteAll()
         fiscalNoteRepository.deleteAll()
+        locationRepository.deleteAll()
 
         val users = listOf(
             User(null,"Marc", "123.123.123-12","marc@gmail.com","(12)1234 1234"),
@@ -43,30 +50,37 @@ class TestConfig(
         )
 
         val products = listOf(
-            Product(null,"123","123456789","Popeye", "Espinaca em pô", "hervas", "kg"),
-            Product(null,"124","123456781","Brutus", "Pinheiro deshidratado","lenha", "unidade")
+            Product(null,"123","123456789","Popeye", "Espinaca em pô", "hervas", UnitMeasures.KG),
+            Product(null,"124","123456781","Brutus", "Pinheiro deshidratado","lenha", UnitMeasures.UNIT)
         )
 
         val providers = listOf(
-            Provider(null,"001","Juridico","000.000/0000-11","001","Bom Oleo","Olinho","Oliveira, 33","olivo@verde.com","Oliva Green","(11)1111 1111"),
-            Provider(null,"002","Juridico","000.000/0000-12","002","Blue Berry","Amora","Oliveira, 34","berry@blue.com","Berry Blue","(22)2222 2222")
+            Provider(null,"001", ProviderType.JURIDIC,"000.000/0000-11","001","Bom Oleo","Olinho","Oliveira, 33","olivo@verde.com","Oliva Green","(11)1111 1111"),
+            Provider(null,"002",ProviderType.JURIDIC,"000.000/0000-12","002","Blue Berry","Amora","Oliveira, 34","berry@blue.com","Berry Blue","(22)2222 2222")
         )
 
         val fiscalNotes = listOf(
             FiscalNote(null,"123456789123456789",1,"123456a", FiscalNoteModel.NFE, Instant.now(),
-                Instant.now(),providers.get(0), BigDecimal("800.00"),BigDecimal("120.00"),BigDecimal("99.00"),BigDecimal("150.00"),BigDecimal("50.00"), products,
+                Instant.now(),providers[0], BigDecimal("800.00"),BigDecimal("120.00"),BigDecimal("99.00"),BigDecimal("150.00"),BigDecimal("50.00"), products,
                 FiscalNoteStatus.AUTORIZED,"0000000000","1111111111",users.get(0))
+        )
+
+        val locations = listOf(
+            Location(null,Sections.TOOLS,'A',1,1),
+            Location(null,Sections.TOOLS,'A',1,2)
         )
 
         userRepository.saveAll(users)
         productRepository.saveAll(products)
         providerRepository.saveAll(providers)
         fiscalNoteRepository.saveAll(fiscalNotes)
+        locationRepository.saveAll(locations)
 
         userRepository.flush()
         productRepository.flush()
         providerRepository.flush()
         fiscalNoteRepository.flush()
+        locationRepository.flush()
 
     }
 }
