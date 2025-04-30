d104306660c75405d00255e0326e2fcc5ecfdc79
vicap <danicapote92@gmail.com>
2025-04-30 20:31:37 -0300

    update: Location class atributes changed, Sections enum created

--- src/main/kotlin/com/angerbytes/almoxarifado/model/Location.kt
+++ src/main/kotlin/com/angerbytes/almoxarifado/model/Location.kt
@@ -1,12 +1,18 @@
 package com.angerbytes.almoxarifado.model
 
-import com.angerbytes.almoxarifado.model.enums.Priority
+import com.angerbytes.almoxarifado.model.enums.Sections
+import jakarta.persistence.*
 
+@Entity
+@Table(name="tb_location")
 data class Location(
-    val code: String,
-    val type: String,
-    val zone: String,
-    val capacity: Double,
-    val blocked: Boolean,
-    val priority: Priority
-)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
+    var section: Sections?,
+    var aisle: Char, // Corridor number
+    var level: Byte, // shelf level
+    var position: Byte // spot on the shelf
+){
+    constructor():this(null, Sections.OTHERS, 'A', 1, 1)
+}
---
+++ src/main/kotlin/com/angerbytes/almoxarifado/model/enums/Sections.kt
@@ -0,0 +1,8 @@
+package com.angerbytes.almoxarifado.model.enums
+
+enum class Sections {
+    EPIs,
+    TOOLS,
+    RAW_MATERIALS,
+    OTHERS
+}
