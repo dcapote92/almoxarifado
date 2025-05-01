02bde4dc843d6d5295311badd0947bede5e78a47
vicap <danicapote92@gmail.com>
2025-04-30 21:31:15 -0300

    update: Movement changed to JPA entity

--- src/main/kotlin/com/angerbytes/almoxarifado/model/Movement.kt
+++ src/main/kotlin/com/angerbytes/almoxarifado/model/Movement.kt
@@ -2,14 +2,25 @@ package com.angerbytes.almoxarifado.model
 
 import com.angerbytes.almoxarifado.model.enums.MovementStatus
 import com.angerbytes.almoxarifado.model.enums.MovementType
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.Instant
 
+@Entity
+@Table(name="tb_movement")
 data class Movement(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long?,
     val type: MovementType,
     val docNumber: String,
-    val dateTime: String,
+    val dateTime: Instant,
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     val user: User,
+    @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true)
     val items: List<Product>,
-    val totalValue: Double,
+    val totalValue: BigDecimal,
     var status: MovementStatus,
     val motive: String,
     val referenceDocument: String
