06bc03d4dd25316a5cfd048e77e8da02e512d04b
vicap <danicapote92@gmail.com>
2025-05-02 19:58:59 -0300

    update: imports optimized

--- src/main/kotlin/com/angerbytes/almoxarifado/model/User.kt
+++ src/main/kotlin/com/angerbytes/almoxarifado/model/User.kt
@@ -1,10 +1,6 @@
 package com.angerbytes.almoxarifado.model
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "tb_user")
