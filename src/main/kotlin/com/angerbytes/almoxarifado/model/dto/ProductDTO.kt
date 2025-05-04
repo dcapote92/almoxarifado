ab0cb2b4e17f2305baa8c61eaea215681c47c581
vicap <danicapote92@gmail.com>
2025-05-04 17:54:57 -0300

    update: ProductDTO adjusted to match Product changes

--- src/main/kotlin/com/angerbytes/almoxarifado/model/dto/ProductDTO.kt
+++ src/main/kotlin/com/angerbytes/almoxarifado/model/dto/ProductDTO.kt
@@ -1,23 +1,33 @@
 package com.angerbytes.almoxarifado.model.dto
 
 import com.angerbytes.almoxarifado.model.Product
-import com.angerbytes.almoxarifado.model.enums.UnitMeasures
+import com.angerbytes.almoxarifado.model.Provider
 import java.io.Serializable
+import java.math.BigDecimal
+import java.time.Instant
 
 data class ProductDTO(
     val id: Long?,
     var name: String,
     var description: String,
     var category: String,
-    var unitMeasure: UnitMeasures?,
-    var price: String
+    var stockQuantity: BigDecimal,
+    var manufactureDate: Instant?,
+    var expirationDate: Instant?,
+    var mainProvider: Provider,
+    var price: String,
+    var active: Boolean
 ): Serializable {
     constructor(product: Product):this(
         product.id,
         product.name,
         product.description,
         product.category,
-        product.unitMeasure,
-        "R$ ${product.price}"
+        product.stockQuantity,
+        product.manufactureDate,
+        product.expirationDate,
+        product.mainProvider,
+        "R$ ${product.unitPrice}",
+        product.active
     )
 }
