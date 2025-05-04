0d4347c33dfc26c51cd3fb08b9bc33c0e797b13f
vicap <danicapote92@gmail.com>
2025-05-04 17:52:11 -0300

    update: Location changed from entity to embeddable

--- src/main/kotlin/com/angerbytes/almoxarifado/model/Location.kt
+++ src/main/kotlin/com/angerbytes/almoxarifado/model/Location.kt
@@ -3,19 +3,15 @@ package com.angerbytes.almoxarifado.model
 import com.angerbytes.almoxarifado.model.enums.Sections
 import jakarta.persistence.*
 
-@Entity
-@Table(name="tb_location")
+
+@Embeddable
 data class Location(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long?,
     var section: Sections?,
     var aisle: Char, // Corridor number
     var level: Byte, // shelf level
     var position: Byte // spot on the shelf
 ){
     constructor():this(
-        null,
         Sections.OUTROS,
         'A',
         1,
